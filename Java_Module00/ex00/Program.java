75830394a597610a6b062ed54cbbca056c8919b0
Alae Ezzaouine <52213438+aladinez@users.noreply.github.com>
2023-09-24 11:42:44 +0000

    ex00 : sum of 6 digits number

---
+++ Java_Module00/ex00/Program.java
@@ -0,0 +1,12 @@
+
+class Program{
+    /**
+     * TODO : verify the use of STRING in main
+     * @param args
+     */
+    public static void main(String[] args){
+        int value = 479598;
+        int ret = (value / 100000) + (value / 10000) % 10 + (value / 1000) % 10 + (value / 100) % 10 + (value / 10) % 10 + value % 10;
+        System.out.println(ret);
+    }
+};
