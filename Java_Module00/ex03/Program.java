fe4a88652db9ee60c834889ce13e541b35374f16
Alae Ezzaouine <52213438+aladinez@users.noreply.github.com>
2024-02-19 08:12:00 +0000

    reading input

--- Java_Module00/ex03/Program.java
+++ Java_Module00/ex03/Program.java
@@ -5,28 +5,33 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String week = sc.nextLine();
+        String week;
         boolean delimiter = true;
         int grade = 9;
 
-        for (int i = 0; delimiter || i < 18; i++) {
+        for (int i = 0; sc.hasNextLine() && i < 18; i++) {
             week = sc.nextLine();
+            System.out.println("Week : " + week);
             if (week.equals("42")){
                 break;
             }
-            sc.useDelimiter(" ");
+            // sc.useDelimiter(" ");
             for (int j = 0; j < 5; j++) {
                 int tmpGrade = sc.nextInt();
+                System.out.println("tmp grade : " + tmpGrade);
                 if (tmpGrade < grade)
                 grade = tmpGrade;
             }
-            System.out.print("Week | ");
+            
+            System.out.println("Week | ");
             for (int a = 0; a < grade; a++) {
                 System.out.print("=");
             }
-            System.out.print(">");
-            sc.close();
-            sc = new Scanner(System.in);
+            System.out.println(">");
+            // sc.close();
+            // sc = new Scanner(System.in);
+            
+            
 
         }
         sc.close();
